7572bd849997909dd4c497868622911e4ff80c8a
aemili <andrea.emili@outlook.com>
2023-02-12 22:00:30 +0100

    C++ folder

---
+++ C++/Somma semplice di due numeri.cpp
@@ -0,0 +1,21 @@
+/*
+* Compiler: https://www.onlinegdb.com/
+* Author: Andrea Emili, andrea.emili@outlook.com/
+* Date: 2023/01/13
+* Version: 0.01
+*/
+
+
+#include <iostream>
+
+int main()
+{
+    int a, b, somma;
+    std::cout << "inserisci il primo numero \n";
+    std::cin >> a;
+    std::cout << "inserisci il secondo numero \n";
+    std::cin >> b;
+    somma = 0;
+    somma = a+b;
+    std::cout << "La somma è " << somma;
+}
